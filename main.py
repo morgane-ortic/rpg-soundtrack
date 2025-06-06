42b059fcf499b9100de989750e941b12027423b8
morgane-ortic <morgane.ortic@proton.me>
2025-06-06 22:30:15 +0200

    total refactor to very simple kivy app using vlc - only play and stop

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-from kivy.core.audio import SoundLoader
+from time import sleep
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+import vlc
 
-class SoundtrackPlayer:
-    def __init__(self):
-        self.track = None
+class AudioPlayer(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        
+        self.player = vlc.MediaPlayer()
+        media = vlc.Media('test.mp3')
+        self.player.set_media(media)
 
-    def load_track(self, file_path):
-        self.track = SoundLoader.load(file_path)
-        if self.track
-            print(f'Track {file_path} loaded successfully :)')
-        else:
-            print(f'Failed to load soundtrack {file_path} :(')
+        self.play_button = Button(text="Play", size_hint=(1, 0.3))
+        self.play_button.bind(on_press=self.play_audio)
+        
+        self.stop_button = Button(text="Stop", size_hint=(1, 0.3))
+        self.stop_button.bind(on_press=self.stop_audio)
 
-    def play(self):
-        if self.track:
-            self.track.play()
+        self.add_widget(self.play_button)
+        self.add_widget(self.stop_button)
 
-    def pause(self):
-        if self.track:
-            self.track.stop()
+    def play_audio(self, instance):
+        self.player.play()
+        Clock.schedule_interval(self.check_playback, 1)  # Check every second
+    
+    def check_playback(self, dt):
+        if self.player.get_state() != vlc.State.Playing:
+            Clock.unschedule(self.check_playback)  # Stop checking when playback finishes
 
-    def stop(self):
-        if self.track:
-            self.track.stop()
-            # resets playing position to file start for next playing
-            self.track.seek(0)
+    def stop_audio(self, instance):
+        self.player.stop()
+        Clock.unschedule(self.check_playback)  # Ensure the scheduled check stops
+
+    def stop_audio(self, instance):
+        self.player.stop()
+
+
+class MyApp(App):
+    def build(self):
+        return AudioPlayer()
+    
+if __name__ == "__main__":
+    MyApp().run()
