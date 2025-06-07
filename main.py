ae513b78928ad7f30d23f620a261bca3c4823d68
morgane-ortic <morgane.ortic@proton.me>
2025-06-07 17:59:17 +0200

    added basic pause - next/previous functions and buttons

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 from time import sleep
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 import vlc
-
 class AudioPlayer(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = 'vertical'
         
-        self.player = vlc.MediaPlayer()
-        media = vlc.Media('test.mp3')
-        self.player.set_media(media)
+        self.list_player = vlc.MediaListPlayer()
+        self.media_list = vlc.MediaList()
+        self.list_player.set_media_list(self.media_list)
 
-        self.play_button = Button(text="Play", size_hint=(1, 0.3))
-        self.play_button.bind(on_press=self.play_audio)
-        
-        self.stop_button = Button(text="Stop", size_hint=(1, 0.3))
-        self.stop_button.bind(on_press=self.stop_audio)
+        self.player = vlc.MediaPlayer()  # Regular media player
+        self.list_player.set_media_player(self.player)  # Link both players
+        self.add_media('test.mp3')
+        self.add_media('test2.m4a')
+        self.add_media('test3.mp3')
 
-        self.add_widget(self.play_button)
-        self.add_widget(self.stop_button)
+        self.paused = False
+
+    def add_media(self, file_path):
+        '''Adds a media file to the list'''
+        media = vlc.Media(file_path)
+        self.media_list.add_media(media)
+        print(f'Added media: {media}')
 
     def play_audio(self, instance):
-        self.player.play()
+        self.paused = False
+        self.list_player.play()
         Clock.schedule_interval(self.check_playback, 1)  # Check every second
     
     def check_playback(self, dt):
@@ -32,16 +37,37 @@ class AudioPlayer(BoxLayout):
             Clock.unschedule(self.check_playback)  # Stop checking when playback finishes
 
     def stop_audio(self, instance):
-        self.player.stop()
+        self.paused = False  # Reset pause tracking
+        self.list_player.stop()
         Clock.unschedule(self.check_playback)  # Ensure the scheduled check stops
 
-    def stop_audio(self, instance):
-        self.player.stop()
+    def pause_audio(self, instance):
+        self.paused = True  # Track paused state
+        self.list_player.pause()
+
+    def next_track(self, instance):
+        '''Skip to the next track'''
+        self.list_player.next()
+        if self.paused:
+            Clock.schedule_once(self.ensure_pause)
+
+    def previous_track(self, instance):
+        '''Go back to the previous track'''
+        self.list_player.previous()
+        if self.paused:
+            Clock.schedule_once(self.ensure_pause)
+
+    def ensure_pause(self, instance):
+        '''Ensure the player is paused only after it's started playing'''
+        if self.player.get_state() == vlc.State.Playing:
+            self.pause_audio(None)
 
 
-class MyApp(App):
+class SimultrackApp(App):
     def build(self):
+        self.title = 'Simultrack'
+        Builder.load_file('audio_player.kv')
         return AudioPlayer()
     
-if __name__ == "__main__":
-    MyApp().run()
+if __name__ == '__main__':
+    SimultrackApp().run()
